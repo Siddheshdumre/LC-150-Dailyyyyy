308281fa3492b0025462238a2b74e01c00aa93a1
Siddheshdumre <90680459+Siddheshdumre@users.noreply.github.com>
2025-07-22 18:30:45 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (24.73%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int>res(nums.size());
+
+
+        int n = nums.size();
+        k = k % n; //for if k > n
+
+        for(int i = 0 ; i<n; i++){
+            res[(i + k) % n] = nums[i];
+
+        }
+     nums = res;
+    }
+};
