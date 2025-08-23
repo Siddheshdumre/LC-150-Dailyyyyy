4f71e7bc99b8da957028e9f584c98806b90e4b38
Siddheshdumre <90680459+Siddheshdumre@users.noreply.github.com>
2025-08-23 16:22:59 +0530

    Time: 0 ms (100.00%) | Memory: 24.4 MB (51.26%) - LeetSync

--- 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency.cpp
+++ 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-    bool isPrime(int n) {
-        if (n <= 1)
+    bool check(int n)
+    {
+        if(n<=1)
+        return false;
+
+        if(n==2||n==3)
+        return true;
+
+        if(n%2==0||n%3==0)
+        return false;
+
+        for(int i=5;i*i<=n;i+=6)
+        {
+            if(n%i==0||n%(i+2)==0)
             return false;
-        if (n <= 3)
-            return true;
-        if (n % 2 == 0 || n % 3 == 0)
-            return false;
-        for (int i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0) {
-                return false;
-            }
         }
+
         return true;
     }
-
     bool checkPrimeFrequency(vector<int>& nums) {
-        unordered_map<int, int> hash;
-        for (auto it : nums) {
-            hash[it]++;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
         }
-        for (auto it : hash) {
-            if (isPrime(it.second)) {
-                return true;
-            }
+
+        for(auto p:mp)
+        {
+            if(check(p.second))
+            return true;
         }
+
         return false;
     }
 };
