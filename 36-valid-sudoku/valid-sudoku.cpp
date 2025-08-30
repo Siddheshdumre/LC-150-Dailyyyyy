f1db3eb1cf551f6211c7465661eb69283fa942f8
Siddheshdumre <90680459+Siddheshdumre@users.noreply.github.com>
2025-08-30 09:21:41 +0530

    Time: 7 ms (35.47%) | Memory: 25.7 MB (23.81%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        bitset<9> Col[9];
-        bitset<9> Row[9];
-        bitset<9> Block[9];
-
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
+        unordered_set<char> row[9], cols[9], boxes[9];
+        for(int i = 0; i<9; i++){
+            for(int j = 0;j<9; j++){
                 char c = board[i][j];
-                if (c == '.') continue;
-                int x=(c-'0')%9; 
-                // Convert the character digit to an index (0-8) 0 for 9
-
-                if (Row[i][x]) return 0;
-                Row[i][x] = 1;
-
-                if (Col[j][x]) return 0;
-                Col[j][x] = 1;
-                
-                int bidx = (i / 3) * 3 + j / 3;
-                if (Block[bidx][x]) return 0;
-                Block[bidx][x] = 1;
+                if(c=='.')continue;
+                int boxidx = (i/3)*3 + (j/3);
+                if(row[i].count(c) || cols[j].count(c) || boxes[boxidx].count(c)) return false;
+                row[i].insert(c);
+                cols[j].insert(c);
+                boxes[boxidx].insert(c);
             }
-        }
-        return 1;
+        } 
+        return true;
     }
 };
