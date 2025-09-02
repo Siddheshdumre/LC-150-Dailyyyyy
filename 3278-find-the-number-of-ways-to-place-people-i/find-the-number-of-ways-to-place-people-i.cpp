e9763e3356482d013ccdf48d56b0259797e43267
Siddheshdumre <90680459+Siddheshdumre@users.noreply.github.com>
2025-09-02 11:13:49 +0530

    Time: 4 ms (73.33%) | Memory: 32.7 MB (39.26%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    static bool cmp(vector<int>& p, vector<int>& q){
-        return (p[0]==q[0])?p[1]<q[1]:p[0]>q[0];
-    }
+    int numberOfPairs(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), [](const auto &a, const auto &b) { return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0]; });
 
-    int numberOfPairs(vector<vector<int>>& P) {
-        sort(P.begin(), P.end(), cmp);
-        int n = P.size(), ans = 0;
-        for(int i=0; i<n-1; i++){
-            int y=INT_MAX;
-            for(int j = i+1; j<n; j++){
-                if (P[j][1]>=P[i][1] && y>P[j][1]){
-                    ans++;
-                    y=P[j][1];
+        int res = 0, n = points.size();
+        for (int i = 0; i < n; i++)
+        {
+            int b = points[i][1], last = -1;
+            for (int j = i + 1; j < n; j++)
+            {
+                int a = points[j][1];
+                if (a <= b && a > last)
+                {
+                    last = a;
+                    ++res;
                 }
             }
         }
-        return ans;
+
+        return res;
     }
 };
-
