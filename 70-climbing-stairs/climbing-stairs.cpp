13064f21651cd67a0eec0d2a998ac79de1e176ab
Siddheshdumre <90680459+Siddheshdumre@users.noreply.github.com>
2025-09-12 21:30:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (87.76%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int first = 1, second = 2;
+
+        if(n<2) return n;
+        for(int i = 3; i<=n; i++){
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+
+    return second;    
+    }
+};
