cb1c4499cb2676eefbd6bcf846969a5f8cf8434b
Siddheshdumre <90680459+Siddheshdumre@users.noreply.github.com>
2025-09-19 13:51:55 +0530

    Time: 2666 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+-- ('Low Salary', 1)
+-- UNION ALL
+-- ('Average Salary', 0)  
+-- UNION ALL
+-- ('High Salary', 3)
+
+SELECT
+    'Low Salary' As category
+    ,COUNT(CASE WHEN income<20000 THEN 1 END) AS accounts_count
+FROM accounts
+
+UNION ALL
+
+SELECT
+    'Average Salary' AS category
+    ,COUNT(CASE WHEN income BETWEEN 20000 AND 50000 THEN 1 END) AS accounts_count
+FROM accounts
+
+UNION ALL
+
+SELECT 
+    'High Salary' AS category
+    ,COUNT(CASE WHEN income>50000 THEN 1 END) AS accounts_count
+FROM accounts
